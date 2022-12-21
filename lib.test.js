b69734312e37f1fee746e38ac909e4fa2af784bd
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-12-21 19:48:25 +0100

    feat: add unit tests for the single password checks

---
+++ lib.test.js
@@ -0,0 +1,50 @@
+/// <reference types="jest" />
+
+import {
+  arePasswordsEqual,
+  containsNumbers,
+  hasLowerCaseLetters,
+  hasUpperCaseLetters,
+  reachesMinimumLength,
+} from "./src/lib";
+
+test("arePasswordEqual should return false when passwords are not equal", () => {
+  const result = arePasswordsEqual("abc", "123");
+  expect(result).toBe(false);
+});
+test("arePasswordEqual should return true when passwords are equal", () => {
+  const result = arePasswordsEqual("abc", "abc");
+  expect(result).toBe(true);
+});
+test("containsNumbers should return false when password 1 doesn't contain a number", () => {
+  const result = containsNumbers("test");
+  expect(result).toBe(false);
+});
+test("containsNumbers should return true when password 1 contains a number", () => {
+  const result = containsNumbers("test1");
+  expect(result).toBe(true);
+});
+test("hasLowerCaseLetters should return false when password 1 doesn't have a lower case letter", () => {
+  const result = hasLowerCaseLetters("TEST");
+  expect(result).toBe(false);
+});
+test("hasLowerCaseLetters should return true when password 1 has a lower case letter", () => {
+  const result = hasLowerCaseLetters("TESt");
+  expect(result).toBe(true);
+});
+test("hasUpperCaseLetters should return false when password 1 doesn't have an uppper case letter", () => {
+  const result = hasUpperCaseLetters("test");
+  expect(result).toBe(false);
+});
+test("hasUpperCaseLetters should return true when password 1 has a lower case letter", () => {
+  const result = hasUpperCaseLetters("Test");
+  expect(result).toBe(true);
+});
+test("reachesMinimumLength should return false when password 1 is shorter than 10 characters", () => {
+  const result = reachesMinimumLength("123456789");
+  expect(result).toBe(false);
+});
+test("reachesMinimumLength should return true when password 1 is at least 10 characters long", () => {
+  const result = reachesMinimumLength("12345678910");
+  expect(result).toBe(true);
+});
--- src/lib.js
+++ src/lib.js
@@ -1,20 +1,17 @@
-export function isPassword(pw) {
-  return pw.getAttribute("type") === "password";
+export function arePasswordsEqual(pw1, pw2) {
+  return pw1 === pw2 ? true : false;
 }
-export function changeType(pw, value) {
-  return pw.setAttribute("type", value);
+export function containsNumbers(pw) {
+  return /\d/.test(pw);
 }
-export function changeText(el, txt) {
-  el.innerText = txt;
+export function hasLowerCaseLetters(pw) {
+  return /[a-z]/.test(pw);
 }
-export function setCheckStatus(check, success) {
-  if (success) {
-    check.classList.add("checks__check--success");
-    check.classList.remove("checks__check--failed");
-  } else {
-    check.classList.remove("checks__check--success");
-    check.classList.add("checks__check--failed");
-  }
+export function hasUpperCaseLetters(pw) {
+  return /[A-Z]/.test(pw);
+}
+export function reachesMinimumLength(pw) {
+  return pw.length >= 10 ? true : false;
 }
 
 export function initApp() {
@@ -26,9 +23,19 @@ export function initApp() {
   const length = document.querySelector("#length");
   const numbers = document.querySelector("#numbers");
   const equal = document.querySelector("#equal");
+  const allTests = document.querySelectorAll(".checks__check");
 
   const btn = document.querySelector("button");
 
+  function changeType(pw, value) {
+    return pw.setAttribute("type", value);
+  }
+  function changeText(el, txt) {
+    el.innerText = txt;
+  }
+  function isPassword(pw) {
+    return pw.getAttribute("type") === "password";
+  }
   function toggleButton() {
     const isInputPassword = isPassword(inputPw1);
     if (isInputPassword) {
@@ -43,24 +50,33 @@ export function initApp() {
   }
   btn.addEventListener("click", toggleButton);
 
+  function setCheckStatus(check, success) {
+    if (success) {
+      check.classList.add("checks__check--success");
+      check.classList.remove("checks__check--failed");
+    } else {
+      check.classList.remove("checks__check--success");
+      check.classList.add("checks__check--failed");
+    }
+  }
+
   function checkPasswords() {
     const pw1 = inputPw1.value;
     const pw2 = inputPw2.value;
 
     if (pw1 !== pw2 || pw1.length === 0 || pw2.length === 0) {
       setCheckStatus(equal, false);
+      setCheckStatus(numbers, false);
       setCheckStatus(lowerCase, false);
       setCheckStatus(upperCase, false);
       setCheckStatus(length, false);
-      setCheckStatus(numbers, false);
       return;
     }
-
-    setCheckStatus(equal, pw1 === pw2);
-    setCheckStatus(numbers, /\d/.test(pw1));
-    setCheckStatus(lowerCase, /[a-z]/.test(pw1));
-    setCheckStatus(upperCase, /[A-Z]/.test(pw1));
-    setCheckStatus(length, pw1.length >= 10);
+    setCheckStatus(equal, arePasswordsEqual(pw1, pw2));
+    setCheckStatus(numbers, containsNumbers(pw1));
+    setCheckStatus(lowerCase, hasLowerCaseLetters(pw1));
+    setCheckStatus(upperCase, hasUpperCaseLetters(pw1));
+    setCheckStatus(length, isLongEnough(pw1));
   }
   inputPw1.addEventListener("keyup", checkPasswords);
   inputPw2.addEventListener("keyup", checkPasswords);
